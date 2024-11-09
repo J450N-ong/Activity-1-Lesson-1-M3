59743ba99bf6b3762181c4afe11a76b48e1f2710
J450N-ong <odeleyejason@gmail.com>
2024-11-09 15:15:11 +0100

    Add files via upload

---
+++ Activity 1.py
@@ -0,0 +1,12 @@
+def wellwishes():
+    print("Hello. How are you doing?")
+wellwishes()
+
+def season(s,w):
+    print("The first season is",s)
+    print("The second season is",w)
+season("summer","winter")
+
+def calc(a,b):
+    return a*b
+print(calc(12,7))
